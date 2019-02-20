7bb4331cc7dc44373bd56191ece445410aab16f9
doDomoGu <kaka_71936410@qq.com>
2019-02-20 17:47:35 +0800

    rest配置 增加登录接口 不需要key

--- application/config/example/rest.php
+++ application/config/example/rest.php
@@ -1,31 +1,32 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-//$config['rest_key_name'] = 'X-API-KEY';
+$config['rest_key_name'] = 'X-TOKEN';
 
 /*
-CREATE TABLE `api_keys` (
+CREATE TABLE `api_token` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `user_id` INT(20) NOT NULL,
     `key` VARCHAR(40) NOT NULL,
     `level` INT(2) NOT NULL,
     `ignore_limits` TINYINT(1) NOT NULL DEFAULT '0',
-    `date_created` INT(11) NOT NULL,
+    `created_time` datetime NOT NULL,
+    `expired_time` datetime NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */
 
-$config['rest_keys_table'] = 'api_keys';
+$config['rest_keys_table'] = 'api_token';
 
 /*
-CREATE TABLE `api_logs` (
+CREATE TABLE `api_log` (
    `id` INT(11) NOT NULL AUTO_INCREMENT,
    `uri` VARCHAR(255) NOT NULL,
    `method` VARCHAR(6) NOT NULL,
    `params` TEXT DEFAULT NULL,
    `api_key` VARCHAR(40) DEFAULT NULL,
    `ip_address` VARCHAR(45) NOT NULL,
-   `time` INT(11) NOT NULL,
+   `time` datetime NOT NULL,
    `rtime` FLOAT DEFAULT NULL,
    `authorized` VARCHAR(1) DEFAULT NULL,
    `response_data` TEXT DEFAULT NULL,
@@ -34,8 +35,12 @@ CREATE TABLE `api_logs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */
 
-$config['rest_logs_table'] = 'api_logs';
+$config['rest_logs_table'] = 'api_log';
 
+//不需要验证 key 的uri列表
 $config['rest_ignore_uris'] = [
-    //'example/user/list'
+    'example/auth/login'
 ];
+
+//api_key 过期时间
+$config['rest_api_key_expired_time'] = 3600;
