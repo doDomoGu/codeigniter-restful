0a54516e76fb69aea729f7dee470b0536ca74dd5
doDomoGu <kaka_71936410@qq.com>
2019-02-25 17:24:05 +0800

    rest.php 配置文件

--- application/config/example/rest.php
+++ application/config/example/rest.php
@@ -1,45 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['rest_key_name'] = 'X-TOKEN';
+$config['rest_key_name'] = 'X-TOKEN';  //在header中传递的token键名
 
-/*
-CREATE TABLE `api_token` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `user_id` INT(20) NOT NULL,
-    `api_key` VARCHAR(100) NOT NULL,
-    `level` INT(2) NOT NULL,
-    `ignore_limits` TINYINT(1) NOT NULL DEFAULT '0',
-    `created_time` datetime NOT NULL,
-    `expired_time` datetime NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-*/
+$config['rest_keys_table'] = 'api_token';  //Token表名
 
-$config['rest_keys_table'] = 'api_token';
+$config['rest_key_column'] = 'api_key'; //Token表中表示key的键名
 
-$config['rest_key_column'] = 'api_key';
+$config['rest_logs_table'] = 'api_log'; //Log表名
 
-/*
-CREATE TABLE `api_log` (
-   `id` INT(11) NOT NULL AUTO_INCREMENT,
-   `uri` VARCHAR(255) NOT NULL,
-   `method` VARCHAR(6) NOT NULL,
-   `params` TEXT DEFAULT NULL,
-   `api_key` VARCHAR(100) DEFAULT NULL,
-   `ip_address` VARCHAR(45) NOT NULL,
-   `time` datetime NOT NULL,
-   `rtime` FLOAT DEFAULT NULL,
-   `authorized` VARCHAR(1) DEFAULT NULL,
-   `response_data` TEXT DEFAULT NULL,
-   `response_code` smallint(3) DEFAULT '0',
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-*/
-
-$config['rest_logs_table'] = 'api_log';
-
-//不需要验证 key 的uri列表
+//不需要验证 key 的uri列表  (单个uri由 [请求方法]|[目录]/[控制其名]/[方法名] 组成)
 $config['rest_ignore_uris'] = [
     'post|example/auth/login',
     'post|example/auth/token-verification'
@@ -49,7 +19,7 @@ $config['rest_ignore_uris'] = [
 $config['rest_api_key_expired_time'] = 3600 * 24;
 
 
-$config['check_cors'] = TRUE;
+$config['check_cors'] = TRUE; //启用跨域
 
 $config['allowed_cors_headers'] = [
     'Origin',
