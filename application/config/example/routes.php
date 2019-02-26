2bcb3dea3a233c46c58964b06807efa3870040cb
doDomoGu <kaka_71936410@qq.com>
2019-02-26 15:57:40 +0800

    配置文件 样例

--- application/config/example/routes.php
+++ application/config/example/routes.php
@@ -4,3 +4,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'error';
 
 $route['404_override'] = 'error/page_404';
+
+$route['translate_uri_dashes'] = TRUE;  //url中的下划线 '_' 会转变为 横折线 '-'
